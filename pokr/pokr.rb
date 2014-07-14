4d37339d3f3098db4f74ad9cc35380519176c414
Anthony Zotti <theillbehaviored@gmail.com>
2014-07-14 14:04:38 -0400

    Auto generate dynamic array of cards and deal hands to player

--- pokr/pokr.rb
+++ pokr/pokr.rb
@@ -8,7 +8,7 @@ class Deck
   end
 
   attr_accessor :full_deck
-  
+
   def shuffle_cards
     @full_deck.shuffle!
   end
@@ -23,11 +23,50 @@ class Deck
   def create_cards
     @card_suites.each do |suite|
       @card_values.each do |value|
-        @full_deck << suite + value
+        @full_deck << value + suite
       end
     end
   end
 end
 
-my_d = Deck.new()
-puts my_d.full_deck
+class DrawHands
+  def initialize(deck_object = Deck.new)
+    @full_deck = deck_object.full_deck
+    @num_of_hands = 4
+    @hands = {}
+    assign_cards_to_hands
+    display_hands
+  end
+  private
+
+  def display_cards(hand)
+    @hands[hand].each do |card|
+      puts card
+    end
+  end
+
+  def display_player_header(hand)
+    puts "\nPlayer #{hand + 1}\n--------"
+  end
+
+
+  def display_hands
+    @num_of_hands.times do |hand|
+      display_player_header(hand)
+      display_cards(hand)
+    end
+  end
+
+  def assign_cards_to_hands
+    @num_of_hands.times { |num| @hands[num] = get_cards }
+  end
+
+  def get_cards
+    cards = []
+    cards_per_hand = 5
+    cards_per_hand.times{ cards << @full_deck.pop }
+    cards
+  end
+end
+
+DrawHands.new
