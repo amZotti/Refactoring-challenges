d8408ce9a71ae89064cdc6b88f173b7a00eaab1d
Anthony Zotti <theillbehaviored@gmail.com>
2014-07-16 17:14:29 -0400

    Hotel rooms can be entered with commas without errors

--- hotel_super_challenge/hotel.rb
+++ hotel_super_challenge/hotel.rb
@@ -3,8 +3,9 @@ class Hotel
     @name = raw_hotel_data[0]
     @city = raw_hotel_data[1]
     @phone_number = raw_hotel_data[2]
-    @number_of_single_beds = raw_hotel_data[3].to_i
-    @number_of_double_beds = raw_hotel_data[4].to_i
+    @number_of_single_beds = raw_hotel_data[3].gsub(",","").to_i
+    @number_of_double_beds = raw_hotel_data[4].gsub(",","").to_i
+
   end
 
   attr_reader :name, :city, :phone_number, :number_of_single_beds, :number_of_double_beds
@@ -13,6 +14,6 @@ class Hotel
     puts
     puts "Phone number: #{self.phone_number}"
     puts "Location: #{self.city}"
-    puts "Number of rooms: #{number_of_single_beds + number_of_double_beds}"
+    puts "Number of rooms: #{self.number_of_single_beds + self.number_of_double_beds}"
   end
 end
